73cffb02299136f3d87f66fb744dcf87b12c181d
mostafamohamed1 <mostafa.mohamed2700@gmail.com>
2021-09-07 07:08:13 +0200

    Modify Cors

--- app.js
+++ app.js
@@ -59,7 +59,11 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.post('/webhook-checkout', bookingController.webhookCheckout);
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
